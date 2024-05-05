0f223c1aece7fdd707829db68b3c1f0d09852b94
Maks Demucki <ya.maksim777d@yandex.ru>
2024-05-05 22:13:39 +0600

    update - sprint 9.3

--- acme_project/acme_project/settings.py
+++ acme_project/acme_project/settings.py
@@ -17,7 +17,10 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'birthday.apps.BirthdayConfig',
     'pages.apps.PagesConfig',
+    'django_bootstrap5',
 ]
+
+MEDIA_ROOT = BASE_DIR / 'media'
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -79,7 +82,7 @@ TIME_ZONE = 'UTC'
 
 USE_I18N = True
 
-USE_L10N = True
+USE_L10N = False
 
 USE_TZ = True
 
